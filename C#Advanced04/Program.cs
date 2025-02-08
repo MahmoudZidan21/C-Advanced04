c07a90c224a6545d57ba4ee6880279b4d8e67b52
NanoChip <NanoChip@DESKTOP-2104TCA>
2025-02-08 07:42:02 +0200

    generic hashtable : Hashtable

--- C#Advanced04/Program.cs
+++ C#Advanced04/Program.cs
@@ -1,6 +1,5 @@
 ﻿namespace C_Advanced04;
-    
-    using System.Collections;
+ using System.Collections;
 
 
 
@@ -50,6 +49,43 @@ internal class Program
 
         //PhoneBook.Remove(null); 
         #endregion
+        #region generic hashtable : Hashtable
+
+        //Dictionary<string, long > PhoneBook = new Dictionary<string, long>();
+
+        //PhoneBook.Add("Ahmed", 11111111);
+        //PhoneBook.Add("Ali"  , 22222222);
+        //PhoneBook.Add("Amr"  , 33333333);
+
+
+
+
+        //bool flag = PhoneBook.Remove("Ahmed", out long result);
+
+        //Console.WriteLine(result);
+        //Console.WriteLine(flag);
+
+
+
+        //foreach (KeyValuePair<string , long> item in PhoneBook)
+        //{
+        //    Console.WriteLine(item);
+        //} 
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
